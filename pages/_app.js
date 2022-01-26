7b898440522569503fcf8f83d21778334a339d15
henokwel <henokwel@outlook.com>
2022-01-26 04:30:12 +0100

    home build started

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider} from 'baseui';
+import { styletron } from './styletron';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <StyletronProvider value={styletron}>
+    <BaseProvider theme={LightTheme}>
+      
+      <Component {...pageProps} />
+    </BaseProvider>
+  </StyletronProvider>
+    
+  )
 }
 
 export default MyApp
